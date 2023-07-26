c8b604ab7189d190f5807aabd1d33bc41379fd15
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2023-07-26 18:06:54 +0700

    .

---
+++ 3.Angular_overview/Color-picker/color-picker-app/src/app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ColorPickerComponent} from "./app.component";
+
+
+
+describe('ColorPickerComponent', () => {
+  let component: ColorPickerComponent;
+  let fixture: ComponentFixture<ColorPickerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ColorPickerComponent ]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ColorPickerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
