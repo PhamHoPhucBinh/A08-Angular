b2ae17c6e4c2fe16f70e104d3ba2052de7b36436
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2023-08-02 19:59:30 +0700

    .

---
+++ 5.Component-Interaction/angular-name-card/src/app/rating-bar/rating-bar.component.ts
@@ -0,0 +1,40 @@
+import {Component, EventEmitter, Input, OnInit, Output, SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'app-rating-bar',
+  templateUrl: './rating-bar.component.html',
+  styleUrls: ['./rating-bar.component.css']
+})
+export class RatingBarComponent implements OnInit {
+  @Input()
+  max = 10;
+  @Input()
+  ratingValue = 5;
+  @Input()
+  showRatingValue = true;
+  @Output()
+  rateChange = new EventEmitter<number>();
+  ratingUnits: Array<IRatingUnit> = [];
+
+  constructor() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if ('max' in changes) {
+      let max = changes.max.currentValue;
+      max = typeof max === 'undefined' ? 5 : max;
+      this.max = max;
+      this.calculate(max, this.ratingValue);
+    }
+  }
+
+  calculate(max, ratingValue){
+    this.ratingUnits = Array.from({length:max}),
+      (_, index) => ({
+        
+      })
+  }
+  ngOnInit(): void {
+  }
+
+}
